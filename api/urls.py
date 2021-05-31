85389fd45eb724241b38ea0311e67b504270e445
nineleven <tim-sania2008@mail.ru>
2021-05-31 03:44:58 +0300

    use django rest api

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_available_texts/', views.get_available_texts_view, name='get_available_texts'),
-    path('get_text/', views.get_text_view, name='get_text'),
-    path('encode_text/', views.encode_text_view, name='encode_text'),
+    path('get_available_texts/', views.SampleTextsView.as_view(), name='get_available_texts'),
+    path('get_text/', views.SpecificTextView.as_view(), name='get_text'),
+    path('encode_text/', views.EncodeTextsView.as_view(), name='encode_text'),
 ]
